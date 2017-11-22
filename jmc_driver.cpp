51bdd1f0904a4e8e1b46f9635fa4c56062e4311b
Dominik Sliwa <dominik.sliwa@toradex.com>
2017-11-22 23:23:45 +0100

    Initial commit

---
+++ jmc_driver.cpp
@@ -0,0 +1,6 @@
+#include "jmc_driver.h"
+
+jmc_driver::jmc_driver()
+{
+
+}
---
+++ jmc_driver.h
@@ -0,0 +1,11 @@
+#ifndef JMC_DRIVER_H
+#define JMC_DRIVER_H
+
+
+class jmc_driver
+{
+public:
+    jmc_driver();
+};
+
+#endif // JMC_DRIVER_H
